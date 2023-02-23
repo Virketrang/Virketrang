881b6186978a6fd20b009b4aa80d4326a98220c2
Sigfred Frey Nørgaard Knudsen <sigfred@virkefeltet.dk>
2023-02-23 19:08:49 +0100

    workflow test

--- packages/colors/src/colors/orange.ts
+++ packages/colors/src/colors/orange.ts
@@ -1,47 +1,45 @@
-import { Singleton } from '../../../../.idea/decorators/singleton';
 import { HEX } from '../types/hex';
 
-@Singleton
-class Orange {
-    public get 50(): HEX {
+abstract class Orange {
+    public static get 50(): HEX {
         return '#FFF3E0';
     }
 
-    public get 100(): HEX {
+    public static get 100(): HEX {
         return '#FFE0B2';
     }
 
-    public get 200(): HEX {
+    public static get 200(): HEX {
         return '#FFCC80';
     }
 
-    public get 300(): HEX {
+    public static get 300(): HEX {
         return '#FFB74D';
     }
 
-    public get 400(): HEX {
+    public static get 400(): HEX {
         return '#FFA726';
     }
 
-    public get 500(): HEX {
+    public static get 500(): HEX {
         return '#FF9800';
     }
 
-    public get 600(): HEX {
+    public static get 600(): HEX {
         return '#FB8C00';
     }
 
-    public get 700(): HEX {
+    public static get 700(): HEX {
         return '#F57C00';
     }
 
-    public get 800(): HEX {
+    public static get 800(): HEX {
         return '#EF6C00';
     }
 
-    public get 900(): HEX {
+    public static get 900(): HEX {
         return '#E65100';
     }
 }
 
-export default new Orange();
+export default Orange;
